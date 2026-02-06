6ec4058b9a51dc0f168f50f9987cf2cc48348a26
vchhavi68 <vchhavi68@gmail.com>
2026-02-06 16:33:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const App = () => {
+  const arr=["A","B","C"]
   return (
-    <div id="main">
-               {/* Do not remove the main div */}
+    <div>
+        <ol id='relativeList'>
+        {arr.map((el,i)=>
+            <li id={`relativeListItem${i+1}`} key={`relativeListItem${i+1}`}>{el}</li>
+        )}
+        </ol>
     </div>
   )
 }
